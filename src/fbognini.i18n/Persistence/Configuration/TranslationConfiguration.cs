1358191208809b91794a1ace258bccb1b9810b1c
Francesco Bognini <info@francescobognini.com>
2022-08-25 13:56:14 +0200

    6.1.5 - fix foreign keys

--- src/fbognini.i18n/Persistence/Configuration/TranslationConfiguration.cs
+++ src/fbognini.i18n/Persistence/Configuration/TranslationConfiguration.cs
@@ -10,8 +10,22 @@ namespace fbognini.i18n.Persistence.Configurations
         {
             builder.HasKey(s => new { s.LanguageId, s.TextId, s.ResourceId });
 
+            builder.Property(x => x.LanguageId)
+                .HasMaxLength(5)
+                .IsFixedLength();
+
+            builder.Property(x => x.TextId)
+                .HasMaxLength(100);
+
+            builder.Property(x => x.ResourceId)
+                .HasMaxLength(50);
+
             builder.Property(x => x.Updated)
                 .HasDefaultValue(new DateTime(1970, 1, 1));
+
+            builder.HasOne(x => x.Language)
+                .WithMany(x => x.Translations)
+                .HasForeignKey(s => new { s.LanguageId });
 
             builder.HasOne(x => x.Text)
                 .WithMany(x => x.Translations)
--- src/fbognini.i18n/Persistence/Migrations/20220825115511_FixForeignKeys.Designer.cs
+++ src/fbognini.i18n/Persistence/Migrations/20220825115511_FixForeignKeys.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using fbognini.i18n.Persistence;
 
 namespace fbognini.i18n.Persistence.Migrations
 {
     [DbContext(typeof(I18nContext))]
-    partial class I18nContextModelSnapshot : ModelSnapshot
+    [Migration("20220825115511_FixForeignKeys")]
+    partial class FixForeignKeys
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

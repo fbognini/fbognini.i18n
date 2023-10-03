e091dcdf46ec63054fb89fad4e69ff372e325d52
Simone Ronzoni <sronzoni99@gmail.com>
2023-10-03 11:14:04 +0200

    dashboard - datatable it language

--- src/fbognini.i18n.Dashboard.Razor/wwwroot/js/site.js
+++ src/fbognini.i18n.Dashboard.Razor/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+    $.extend(true, $.fn.dataTable.defaults, {
+        language: {
+            url: "/_content/fbognini.i18n.Dashboard/lib/Datatables/it-IT.json"
+        },
+    });
+});
